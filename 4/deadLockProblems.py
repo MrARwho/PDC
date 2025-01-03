d104d162974ed5801571e7a7f7d2b34341c5ea7a
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2025-01-04 03:01:09 +0500

    Add code to demonstrate deadlocks with point-to-point communication using MPI

---
+++ 4/deadLockProblems.py
@@ -0,0 +1,28 @@
+/*************  ✨ Codeium Command 🌟  *************/
+# Commit message: Add code to demonstrate deadlocks with point-to-point communication using MPI
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.rank
+
+print("my rank is %i" % (rank))
+
+if rank == 1:
+    data_send = "a"
+    destination_process = 5
+    source_process = 5 
+    comm.send(data_send, dest=destination_process) 
+    data_received = comm.recv(source=source_process) 
+    print("sending data %s to process %d" % (data_send, destination_process))
+    print("data received is = %s" % data_received)
+
+if rank == 5:
+    data_send = "b"
+    destination_process = 1
+    source_process = 1
+    comm.send(data_send, dest=destination_process)
+    data_received = comm.recv(source=source_process)
+    print("sending data %s to process %d" % (data_send, destination_process))
+    print("data received is = %s" % data_received)
+
+/******  8bec3aa4-d6c9-4f9e-bd57-5b66a66dd340  *******/
